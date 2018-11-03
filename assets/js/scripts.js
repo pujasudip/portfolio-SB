f257176a3e36e322694aea8300c50ba2dc6c4596
Sudip Baral <pujasudip@gmail.com>
2018-11-03 11:39:59 -0700

    added resume and github link

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 	
 	$('#home').height($(window).height()+50);
 	
-	$.backstretch('assets/images/header-bg.jpg');
+	$.backstretch('assets/images/leafline.jpg');
 	
 	if($(window).scrollTop() > ($(window).height()+50)){
 		$('.backstretch').hide();
@@ -242,57 +242,7 @@ $(document).ready(function() {
 	$('.close-preview').click(function(){
 		closeProject();
 	})
-	
-	/*============================================
-	Twitter
-	==============================================*/
-	var tweetsLength = $('#twitter-slider').data('tweets-length'),
-		widgetID = $('#twitter-slider').data('widget-id');
-	
-	var configTweets = {
-	  "id": widgetID,
-	  "domId": '',
-	  "maxTweets": tweetsLength,
-	  "enableLinks": true,
-	  "showUser": false,
-	  "showTime": true,
-	  "dateFunction": '',
-	  "showRetweet": false,
-	  "customCallback": handleTweets,
-	  "showInteraction": true
-	};
-	
-	twitterFetcher.fetch(configTweets);
 
-	function handleTweets(tweets){
-	
-		var x = tweets.length,
-			n = 0,
-			tweetsHtml = '<ul class="slides">';
-			
-		while(n < x) {
-			tweetsHtml += '<li>' + tweets[n] + '</li>';
-			n++;
-		}
-		
-		tweetsHtml += '</ul>';
-		$('#twitter-slider').html(tweetsHtml);
-	
-		$('.twitter_reply_icon').html("<i class='fa fa-reply'></i>");
-		$('.twitter_retweet_icon').html("<i class='fa fa-retweet'></i>");
-		$('.twitter_fav_icon').html("<i class='fa fa-heart'></i>");
-	  
-		$('#twitter-slider').flexslider({
-			prevText: '<i class="fa fa-angle-left"></i>',
-			nextText: '<i class="fa fa-angle-right"></i>',
-			slideshowSpeed: 5000,
-			useCSS: true,
-			controlNav: false, 
-			pauseOnAction: false, 
-			pauseOnHover: true,
-			smoothHeight: false
-		});
-	}
 	/*============================================
 	Contact Map
 	==============================================*/
--- contact.php
+++ contact.php
@@ -1,67 +1,57 @@
 <?php
+require_once('contactCredential.php');
+require('phpmailer/PHPMailer/PHPMailerAutoload.php');
 
-header('Access-Control-Allow-Origin: *');
-
-$emailTo = 'pujasudip@gmail.com';
-$siteTitle = 'www.codingbaral.com';
-
-error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
-
-//If the form is submitted
-if(isset($_POST['submitted'])) {
-	
-	// require a name from user
-	if(trim($_POST['contactName']) === '') {
-		$nameError =  'Forgot your name!'; 
-		$hasError = true;
-	} else {
-		$name = trim($_POST['contactName']);
-	}
-	
-	// need valid email
-	if(trim($_POST['email']) === '')  {
-		$emailError = 'Forgot to enter in your e-mail address.';
-		$hasError = true;
-	} else if (!preg_match("/^[[:alnum:]][a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,4}$/i", trim($_POST['email']))) {
-		$emailError = 'You entered an invalid email address.';
-		$hasError = true;
-	} else {
-		$email = trim($_POST['email']);
-	}
-		
-	// we need at least some content
-	if(trim($_POST['comments']) === '') {
-		$commentError = 'You forgot to enter a message!';
-		$hasError = true;
-	} else {
-		if(function_exists('stripslashes')) {
-			$comments = stripslashes(trim($_POST['comments']));
-		} else {
-			$comments = trim($_POST['comments']);
-		}
-	}
-		
-	// upon no failure errors let's email now!
-	if(!isset($hasError)) {
-		
-		$subject = 'New message to '.$siteTitle.' from '.$name;
-		$sendCopy = trim($_POST['sendCopy']);
-		$body = "Name: $name \n\nEmail: $email \n\nMessage: $comments";
-		$headers = 'From: ' .' <'.$email.'>' . "\r\n" . 'Reply-To: ' . $email;
-
-		mail($emailTo, $subject, $body, $headers);
-		
-        //Autorespond
-		$respondSubject = 'Thank you for contacting '.$siteTitle;
-		$respondBody = "Your message to $siteTitle has been delivered! \n\nWe will answer back as soon as possible.";
-		$respondHeaders = 'From: ' .' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $emailTo;
-		
-		mail($email, $respondSubject, $respondBody, $respondHeaders);
-		
-        // set our boolean completion value to TRUE
-		$emailSent = true;
-	}
+foreach($_POST as $key=>$value){
+    $_POST[$key] = htmlentities(addslashes($value));
 }
 
-print($_POST['contactName']);
+$mail = new PHPMailer;
+$mail->SMTPDebug = 0;           // Enable verbose debug output. Change to 0 to disable debugging output.
+
+$mail->isSMTP();                // Set mailer to use SMTP.
+$mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers.
+$mail->SMTPAuth = true;         // Enable SMTP authentication
+
+
+$mail->Username = EMAIL_USER;   // SMTP username
+$mail->Password = EMAIL_PASS;   // SMTP password
+$mail->SMTPSecure = 'tls';      // Enable TLS encryption, `ssl` also accepted, but TLS is a newer more-secure encryption
+$mail->Port = 587;              // TCP port to connect to
+$options = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+);
+$mail->smtpConnect($options);
+$mail->From = 'sdipab36@gmail.com';  // sender's email address (shows in "From" field)
+$mail->FromName = $_POST['name'];   // sender's name (shows in "From" field)
+$mail->addAddress('pujasudip@gmail.com', 'Coding Baral');  // Add a recipient
+//$mail->addAddress('ellen@example.com');                        // Name is optional
+$mail->addReplyTo($_POST['email']);                          // Add a reply-to address
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Message from '.$_POST['name'];
+$mail->Body    = "
+                    time: ".date('Y-m-d H:is:s')."
+                    remote addr: {$_SERVER['REMOTE_ADDR']}<br>
+                    name: {$_POST['name']}<br>
+                    email:{$_POST['email']}<br>
+                    message: {$_POST['message']}<br>
+                    ";
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
 ?>
